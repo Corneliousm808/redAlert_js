11dcda9a9272931b3e87dbb7c298e4d53fcf2ae5
Corneliousm808 <corneliousmooring@gmail.com>
2016-12-09 09:02:44 -1000

    so fast

--- js/app.js
+++ js/app.js
@@ -3,5 +3,13 @@ console.log("sanity check")
 function hello () {
   console.log("hello");
   var changeDiv = document.getElementById('divElement');
+  changeDiv.innerHTML = "Wat";
   console.log(changeDiv);
+}
+
+function soFast() {
+  console.log('Hello')
+  var classElements = document.getElementsById('firtsPeriod');
+  console.log(classElements);
+  classElements[0].innerHTML = 'Wat'
 }
